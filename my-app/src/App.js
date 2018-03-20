d69ce195484aa4dac640e7f08cde23ec022d5bcd
Byron Griffith <byrongriffith91@gmail.com>
2018-03-20 18:00:43 -0400

    pages and routes added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import home from "./pages/home";
+import about from "./pages/about";
+import contact from "./pages/contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          
+
+          <Route exact path="/" component={home} />
+          <Route exact path="/about" component={about} />
+          <Route exact path="/contact" component={contact} />
+
+        </div>
+      </Router>
     );
   }
 }
